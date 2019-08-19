27c6fc57f03d4b8138356a74f66b0bbb81781780
Victor Zhang <victorzw895@gmail.com>
2019-08-19 19:09:46 +1000

    Filter functioning properly for categories and tags

--- src/components/SearchDripples.js
+++ src/components/SearchDripples.js
@@ -6,16 +6,37 @@ const CATEGORY_URL = 'http://localhost:3000/api/categories.json';
 // const CATEGORY_URL = 'http://www.dripples.herokuapp.com/api/categories.json';
 const TAG_URL = 'http://localhost:3000/api/tags.json';
 // const TAG_URL = 'http://www.dripples.herokuapp.com/api/tags.json';
-
+const DRIPPLE_URL = 'http://localhost:3000/api/dripples.json'
 
 class SearchDripples extends Component {
     constructor() {
         super();
         this.state = {
-            category: '',
-            tag: ''
+            dripples: []
         }
+        this.saveSearch = this.saveSearch.bind( this );
     }
+
+
+    saveSearch(c_id, t_id) {
+        const token = "Bearer " + localStorage.getItem("jwt");
+        console.log(c_id, t_id);
+        axios({method: 'get', url: DRIPPLE_URL, headers: {'Authorization': token}}).then(
+            response => {
+                const categoryFilter = c_id === 0 ? 
+                    response.data : response.data.filter(
+                        fdp => fdp.category_id === c_id
+                        );
+                const filtered = t_id === 0 ? 
+                    categoryFilter : categoryFilter.filter(
+                        fdp => {
+                            return fdp.tag.some(t => t.id === t_id) 
+                        });
+                this.setState({dripples: filtered})
+            }
+        );
+    }
+    
 
     render() {
         return (
@@ -23,15 +44,7 @@ class SearchDripples extends Component {
                 <SideNavBar />
                 <div className="content">
                     <SearchForm onSubmit={ this.saveSearch }/>
-                    <SearchResults />
-                    <p>should be a component that renders a filter form</p>
-                    <p>user selects filter options and clicks on search</p>
-                    <p>
-                        match dripple with a dripple from another user that has similar matches
-                    </p>
-                    <p>
-                        this should remove all dripples in current state and fill with new dripples from multiple users
-                    </p>
+                    <SearchResults filteredDripples={ this.state.dripples }/>
                 </div>
             </div>
         )
@@ -44,6 +57,8 @@ class SearchForm extends Component {
         this.state = {
             categories: [],
             tags: [],
+            category_id: 0,
+            tag_id: 0,
         }
 
         this._handleSubmit = this._handleSubmit.bind( this );
@@ -63,18 +78,23 @@ class SearchForm extends Component {
         })
     }
 
+    
     _handleSubmit(e) {
         e.preventDefault()
-        // this.props.onSubmit(this.state)
         console.log(this.state);
+        this.props.onSubmit(this.state.category_id, this.state.tag_id)
     }
-
+    
     _handleCategoryChange(e) {
-        this.setState({ categories: e.target.value })
+        const selectedIndex = e.target.options.selectedIndex;
+        console.log(selectedIndex);
+        this.setState({ category_id: selectedIndex })
     }
 
     _handleTagChange(e) {
-        this.setState({ tags: e.target.value})
+        const selectedIndex = e.target.options.selectedIndex;
+        console.log(selectedIndex);
+        this.setState({ tag_id: selectedIndex})
     }
 
     render() {
@@ -84,7 +104,7 @@ class SearchForm extends Component {
                 <select onChange={ this._handleCategoryChange }>
                     <option>None</option>
                     {this.state.categories.map((c) => 
-                        <option key={c.id}>{c.name}</option>
+                        <option key={c.id} data-key={c.id}>{c.name}</option>
                     )}
                 </select>
 
@@ -92,7 +112,7 @@ class SearchForm extends Component {
                 <select onChange={ this._handleTagChange }>
                     <option>None</option>
                     {this.state.tags.map((t) => 
-                        <option key={t.id}>{t.tag_name}</option>
+                        <option key={t.id} data-key={t.id}>{t.tag_name}</option>
                     )}
                 </select>
                 <button type="submit">Search</button>
@@ -102,10 +122,45 @@ class SearchForm extends Component {
 }
 
 class SearchResults extends Component {
+    constructor() {
+        super();
+        this.state = {
+            featured_id: null,
+            featured: false
+        }
+        this._handleClick = this._handleClick.bind( this );
+    }
+
+    _handleClick(i) {
+        const { featured_id, featured } = this.state
+        if (featured_id === null && !featured) {
+            this.setState({featured_id: i, featured: !featured})
+        } else if (featured && featured_id === i ) {
+            this.setState({featured_id: null, featured: !featured})
+        }
+    }
+
     render() {
+        const { featured_id, featured } = this.state;
+        let controlOptions;
+        if (featured) {
+            controlOptions = (
+                <div>
+                    {/* MAYBE */}
+                    {/* <ConnectDripple drippleId={ featured_id } onSubmit={ this._handleConnect } /> */}
+                </div>
+            )
+        }
         return (
             <div>
-                should be dripples results from filtering
+                {this.props.filteredDripples.map((dp) => 
+                    <div className="dripple" key={dp.id}>
+                        <div key={dp.id} onClick={() => this._handleClick(dp.id)}>
+                                {dp.title} {dp.content}
+                        </div>
+                        {dp.id === featured_id ? controlOptions : null}
+                    </div>
+                    )}
             </div>
         )
     }
