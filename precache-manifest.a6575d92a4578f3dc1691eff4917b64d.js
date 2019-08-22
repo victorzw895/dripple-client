ed143399924780df1d7a0cb6cac4c41c2d4a9cf7
Meo So <meowsoso@gmail.com>
2019-08-22 19:40:02 +1000

    Updates

---
+++ precache-manifest.a6575d92a4578f3dc1691eff4917b64d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2cd7722e3cc1408f84f7f26f75632ad",
+    "url": "/dripple-client/index.html"
+  },
+  {
+    "revision": "c09e97422e0e8c4a01dd",
+    "url": "/dripple-client/static/css/main.e475ab52.chunk.css"
+  },
+  {
+    "revision": "d3286700432fb5e8c70a",
+    "url": "/dripple-client/static/js/2.d0813816.chunk.js"
+  },
+  {
+    "revision": "c09e97422e0e8c4a01dd",
+    "url": "/dripple-client/static/js/main.d56b865c.chunk.js"
+  },
+  {
+    "revision": "f6a05898bbafb6b3b94c",
+    "url": "/dripple-client/static/js/runtime~main.db9f95a8.js"
+  },
+  {
+    "revision": "c4d6be1a6936cff016f232de74e7ecae",
+    "url": "/dripple-client/static/media/ripplebg.c4d6be1a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/repository-name/precache-manifest.c72ca33ad923d5d102b1aec380883bcd.js"
+  "/dripple-client/precache-manifest.a6575d92a4578f3dc1691eff4917b64d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/repository-name/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dripple-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
